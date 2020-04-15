1c7f95f2ce8e23f1e75e8b1ffa783a1782d27428
pietrantonioalessandro <pietrantonio.alessandro@gmail.com>
2020-04-16 00:26:38 +0200

    donazione paypal

--- resources/views/parts/header.blade.php
+++ resources/views/parts/header.blade.php
@@ -6,7 +6,15 @@
         aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
+    <div class="collapse justify-content-between navbar-collapse show" id="navbarNav">
         @yield('navbar-nav')
+        <div>
+            <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+                <input type="hidden" name="cmd" value="_s-xclick" />
+                <input type="hidden" name="hosted_button_id" value="UAWNTS5F6W5TQ" />
+                <button class="btn btn-gradient" type="submit" name="submit" title="PayPal - The safer, easier way to pay online!"
+                alt="Donate with PayPal button">☕ Offrimi un caffè!</button>
+            </form>
+        </div>
     </div>
 </nav>
