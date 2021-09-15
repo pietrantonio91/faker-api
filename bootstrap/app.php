23d3707da69b8d0d51638fe981c02ba9310bfee3
pietrantonio-portatile <pietrantonio.alessandro@gmail.com>
2021-09-15 22:28:59 +0200

    fix ResponseFactory

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -101,7 +101,7 @@ $app->routeMiddleware([
 |
 */
 
-// $app->register(App\Providers\AppServiceProvider::class);
+$app->register(App\Providers\AppServiceProvider::class);
 // $app->register(App\Providers\AuthServiceProvider::class);
 // $app->register(App\Providers\EventServiceProvider::class);
 $app->register(Illuminate\Redis\RedisServiceProvider::class);
